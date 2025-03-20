1c32897b20dd3edfd0a1ad7765e88a3f0f7bd9f2
leandroofaria <leandrogabrielfaria05@gmail.com>
2025-03-20 12:29:42 -0300

    erro

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,5 +1,28 @@
+import Link from "next/link"
+
 export function Header() {
     return (
-        <h1>Header</h1>
+        <header className="w-full h-20 px-2">
+            <div className="max-w-screen-xl mx-auto flex px-8 justify-center sm:justify-between items-center h-20">
+                <nav className="">
+                    <Link href="/">ScoreIt</Link>
+                    
+                    <div>
+                        <Link href="/filmes">Filmes</Link>
+                        <Link href="/musicas">Músicas</Link>
+                    </div>
+                </nav>
+
+                <div className="">
+                    <Link href="/profile">
+                    <p>Meu perfil</p>
+                    </Link>
+
+                    <button>
+                        Logout
+                    </button>
+                </div>
+            </div>
+        </header>
     )
 }
