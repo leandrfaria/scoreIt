3a74c4d18cbee360469215891cafdc2486a3db6f
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2025-04-26 01:38:00 -0300

    Paginação de Filmes

--- frontend/src/components/movie-carousel/MovieCarousel.tsx
+++ frontend/src/components/movie-carousel/MovieCarousel.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from "react"
 import { ArrowLeftIcon as IconArrowLeft, ArrowRightIcon as IconArrowRight } from "lucide-react"
-import { MovieCard } from "@/components/film-card/MovieCard"
+import { MovieCard } from "@/components/movie-card/MovieCard"
 import type { Movie } from "@/types/Movie"
 
 
