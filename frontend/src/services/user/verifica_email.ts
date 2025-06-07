4f41d5e7af96fcd9afa1f7168291757fe4b8c170
thiagoluzvilv <thiagoluz959@gmail.com>
2025-06-07 19:01:53 -0300

    Verificação de Email

---
+++ frontend/src/services/user/verifica_email.ts
@@ -0,0 +1,19 @@
+export const sendVerificaEmail = async (email: string) => {
+    try {
+      const formData = new URLSearchParams();
+      formData.append("email", email);
+  
+      const response = await fetch("http://localhost:8080/member/confirm", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: formData.toString(),
+      });
+  
+      return { success: true, message: "E-mail enviado com sucesso!" };
+    } catch (error: any) {
+      return { success: false, message: "Erro ao enviar e-mail de redefinição." };
+    }
+  };
+  
