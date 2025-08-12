2cd3a13630c65a1ba8afefe09660b26af50916ac
leandroofaria <leandrogabrielfaria05@gmail.com>
2025-08-12 12:43:07 -0300

    Build Analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,23 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
+import bundleAnalyzer from "@next/bundle-analyzer";
 
-const withNextIntl = createNextIntlPlugin('./next-intl.config.ts');
+const withNextIntl = createNextIntlPlugin("./next-intl.config.ts");
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "image.tmdb.org",
-      },
-      {
-        protocol: "https",
-        hostname: "i.scdn.co",
-      },
-      {
-        protocol: "https",
-        hostname: "marketup.com",
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lastfm.freetls.fastly.net", 
-      },
+      { protocol: "https", hostname: "image.tmdb.org" },
+      { protocol: "https", hostname: "i.scdn.co" },
+      { protocol: "https", hostname: "marketup.com" },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "lastfm.freetls.fastly.net" },
     ],
   },
 };
 
-export default withNextIntl(nextConfig);
+export default withBundleAnalyzer(withNextIntl(nextConfig));
